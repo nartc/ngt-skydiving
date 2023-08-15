1e515993954eff65ebaa9ed7d05a7801c4a564d1
Chau Tran <nartc7789@gmail.com>
2023-08-15 15:26:18 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	template: ``,
+	standalone: true,
+	styles: [],
+})
+export class AppComponent {
+	title = 'ngt-skydiving';
+}
